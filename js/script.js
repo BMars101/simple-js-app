ebb998493bfa5e4ef87bc18291103b5c11bfa826
Beth Day <69202254+BMars101@users.noreply.github.com>
2020-09-14 12:29:26 -0400

    Updated format
    Installed prettier-atom to format code.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-let pokemonList = [{name: "weedle", height: 1, type: ["bug, poison"]},
-{name: "ivysaur", height: 3, type: ["grass", "poison"]},
-{name: "butterfree", height: 3, type: ["bug", "flying"]}]
+let pokemonList = [
+  { name: "weedle", height: 1, type: ["bug, poison"] },
+  { name: "ivysaur", height: 3, type: ["grass", "poison"] },
+  { name: "butterfree", height: 3, type: ["bug", "flying"] },
+];
