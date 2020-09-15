f37a6cfc2e5b81dc0a923c164348fc8c90f3f447
Beth Day <69202254+BMars101@users.noreply.github.com>
2020-09-15 16:27:27 -0400

    Created for loop
    Created loop to display each object on webpage and added code to indicate a particular quality.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ let pokemonList = [
   { name: "ivysaur", height: 3, type: ["grass", "poison"] },
   { name: "butterfree", height: 3, type: ["bug", "flying"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 3) {
+    document.write(
+      pokemonList[i].name + "" + " (height: " + pokemonList[i].height + ") "
+    );
+  } else {
+    document.write(
+      pokemonList[i].name +
+        "" +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        " -this one is smaller! "
+    );
+  }
+}
