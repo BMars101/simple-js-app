8dccf95895206b5db46c29175874e9f36d88327e
Beth Day <69202254+BMars101@users.noreply.github.com>
2020-09-27 10:08:41 -0400

    updated functions in IIFE
    - Created functions within IIFE
    - Added new pokemon to original list
    - Added call commands to display list items

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
+let pokemonList = [
+  {
+    name: "weedle",
+    height: 1,
+    type: ["bug", "poison"],
+  },
+  {
+    name: "ivysaur",
+    height: 3,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "butterfree",
+    height: 3,
+    type: ["bug", "flying"],
+  },
+];
+
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "weedle",
-      height: 1,
-      type: ["bug", "poison"],
-    },
-    {
-      name: "ivysaur",
-      height: 3,
-      type: ["grass", "poison"],
-    },
-    {
-      name: "butterfree",
-      height: 3,
-      type: ["bug", "flying"],
-    },
-  ];
+  let list = [];
+
+  function add(pokemon) {
+    list.push(pokemon);
+  }
 
   function myPokeList(pokemon) {
     console.log(
@@ -27,21 +33,34 @@ let pokemonRepository = (function () {
     );
   }
 
-  pokemonList.forEach(myPokeList);
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
   function getAll() {
-    return pokemonList;
+    return list;
   }
 
   return {
     add: add,
+    myPokeList: myPokeList,
     getAll: getAll,
   };
 })();
-
-pokemonRepository.add({ name: "Venusaur" });
-console.log(pokemonRepository.getAll());
+//add external pokemonList to IIFE pokemonRepository
+pokemonList.forEach(function (pokemon) {
+  pokemonRepository.add(pokemon);
+});
+//create new variable and assign value of pokemonRepository
+let allPokemon = pokemonRepository.getAll();
+//display list
+console.log("my initial pokemon list: ", allPokemon);
+//create new pokemon item
+let newPokemon = {
+  name: "venusaur",
+  height: 6,
+  type: ["poison", "grass"],
+};
+//add new pokemon to list
+pokemonRepository.add(newPokemon);
+console.log("my new pokemon list: ", allPokemon);
+//print list with item details with forEach function
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.myPokeList(pokemon);
+});
